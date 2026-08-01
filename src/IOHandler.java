756f32461c4a0dffe6290fa2e59923a5e592df11
nasccped <pedro.main.tec@gmail.com>
2026-08-01 15:03:53 +0000

    chore: put into package + turn public (extra class call)

--- src/IOHandler.java
+++ src/IOHandler.java
@@ -1,7 +1,9 @@
+package fundb;
+
 import java.util.Scanner;
 
 // Handles the terminal input / output data.
-class IOHandler {
+public class IOHandler {
 
     // Private scanner for user inputing.
     private static final Scanner sc = new Scanner(System.in);
@@ -17,20 +19,20 @@ class IOHandler {
 
     // Prints the left gap spacing (if needed) + obj content to the stdout with a new line at the
     // end.
-    protected static void println(Object obj) {
+    public static void println(Object obj) {
         System.out.println(getLeftGapString() + obj);
         printGap = true;
     }
 
     // Just prints an empty line to stdout.
-    protected static void println() {
+    public static void println() {
         System.out.println();
         printGap = true;
     }
 
     // Prints the left gap spacing (if needed) + obj content to the stdout without a new line at
     // the end.
-    protected static void print(Object obj) {
+    public static void print(Object obj) {
         System.out.print(getLeftGapString() + obj);
         printGap = false;
     }
@@ -47,7 +49,7 @@ class IOHandler {
 
     // Prompt a question to the user with the `p` prefix. This function prints the left gap if
     // necessary and returns the final String.
-    protected static String prompt(String p) {
+    public static String prompt(String p) {
         System.out.print(getLeftGapString() + p);
         printGap = true;
         return sc.nextLine();
@@ -55,7 +57,7 @@ class IOHandler {
 
     // Prompt a question to the user without any prefix. This function doesn't prints the left gap
     // and returns the final String.
-    protected static String prompt() {
+    public static String prompt() {
         printGap = true;
         return sc.nextLine();
     }
