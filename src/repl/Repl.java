b1d7e7881dbf4461a87526981b43d1a4ce802ff8
nasccped <pedro.main.tec@gmail.com>
2026-08-04 00:15:37 +0000

    chore: add run-terminate features

--- src/repl/Repl.java
+++ src/repl/Repl.java
@@ -5,10 +5,14 @@ import fundb.repl.printer.Printer;
 // Class responsible for repl (read, evaluate, print and loop) stuff.
 public class Repl {
 
+    // If the program is currently running.
+    private boolean looping;
+
     // Does the printing to sysout.
     private Printer printer;
 
     public Repl() {
+        this.looping = true;
         this.printer = new Printer();
     }
 
@@ -17,4 +21,13 @@ public class Repl {
         printer.printWelcome();
     }
 
+    // Returns if the program is currently running.
+    public boolean isLooping() {
+        return looping;
+    }
+
+    // Turns the program off by set looping to `false`.
+    public void terminate() {
+        looping = false;
+    }
 }
