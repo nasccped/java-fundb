fe555bb8e6e657131d66c2162743b904f192cedd
nasccped <pedro.main.tec@gmail.com>
2026-08-04 00:16:14 +0000

    chore: call latest repl feats (b1d7e78)

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,15 @@ public class Main {
     public static void main(String[] args) {
         // Prints the welcome message to the user through sysout.
         repl.printWelcome();
+
+        int counter = 1;
+
+        while (repl.isLooping()) {
+            counter++;
+            if (counter >= 10)
+                repl.terminate();
+        }
+
+        System.out.println("Current counter value: " + counter);
     }
 }
