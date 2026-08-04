4758285968c55dac39b89fb8a4d302cad351747a
nasccped <pedro.main.tec@gmail.com>
2026-08-04 00:06:26 +0000

    fix(doc): remove controversal comment

--- src/repl/Repl.java
+++ src/repl/Repl.java
@@ -3,9 +3,6 @@ package fundb.repl;
 import fundb.repl.printer.Printer;
 
 // Class responsible for repl (read, evaluate, print and loop) stuff.
-//
-// This class exposes all the necessary methods for dealing with input/output work, but the
-// `iohandler` package holds the std(io) implementations.
 public class Repl {
 
     // Does the printing to sysout.
