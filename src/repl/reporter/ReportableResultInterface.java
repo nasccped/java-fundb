0e9030f43403f591ff5115d790de76da295609d0
nasccped <pedro.main.tec@gmail.com>
2026-08-08 15:20:55 +0000

    moved: now refers to 'MayReportInterface'

--- src/repl/reporter/ReportableResultInterface.java
+++
@@ -1,8 +0,0 @@
-package fundb.repl.reporter;
-
-// Means any result type (even exceptions) that can be reported through `Reporter` object.
-public interface ReportableResultInterface {
-
-    // Returns the report kind associated with the self object.
-    public ReportKind getReportKind();
-}
