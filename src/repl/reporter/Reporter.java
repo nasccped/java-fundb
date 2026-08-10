62cd16060d424b732bc65658ee03aba4a2f95838
nasccped <pedro.main.tec@gmail.com>
2026-08-10 19:52:30 +0000

    chpre: implement reporting logic

--- src/repl/reporter/Reporter.java
+++ src/repl/reporter/Reporter.java
@@ -1,6 +1,12 @@
 package fundb.repl.reporter;
 
 import fundb.repl.printer.Printer;
+import fundb.utils.reportable.Report;
+import fundb.utils.reportable.ReportKind;
+import fundb.utils.reportable.MayReportInterface;
+import fundb.utils.reportable.MultilineContentInterface;
+import fundb.utils.strings.AsStringReprInterface;
+import java.util.Optional;
 
 public class Reporter {
 
@@ -12,18 +18,45 @@ public class Reporter {
     }
 
     // Does the actual reporting.
-    public void reportResult(ReportableResultInterface result) {
+    public void reportResult(MayReportInterface result) {
+        Report report;
+        ReportKind kind;
+        int initialGap;
 
-        // ignore if quiet report.
-        if (!shouldBeReported(result))
+        // null data means no data
+        if ((report = result.mayReport().orElse(null)) == null)
             return;
 
-        // TODO: implement error reporting logic.
-        throw new UnsupportedOperationException("TODO: implement reporting logic");
-    }
+        initialGap = printer.getLeftGap();
 
-    // Returns if the given result should be reported to sysout.
-    private boolean shouldBeReported(ReportableResultInterface result) {
-        return result.getReportKind() != ReportKind.QUIET;
+        // if report kind expected to be displayed (not anonymous).
+        if ((kind = report.getReportKind()).isKindRepresentable()) {
+
+            printer.print(kind.asStringRepr());
+            printer.print(' ');
+
+            // update new gap for content-aligned printing.
+            printer.setLeftGap(initialGap + kind.getStringLength() + 1);
+        }
+
+        // Print title for the current report.
+        printer.println(report.getTitleString());
+
+        // Print detail if necessary.
+        report.optionalDetail().ifPresent(detail -> {
+            printer.println(detail.getDetailString());
+        });
+
+        report.optionalMultilineContent().ifPresent(mc -> {
+            // gap from previous detail.
+            printer.println();
+
+            for (AsStringReprInterface row : mc.getContentLines())
+                printer.println(row.asStringRepr());
+        });
+
+        // 1 row gap for next reading + reset left gap.
+        printer.println();
+        printer.setLeftGap(initialGap);
     }
 }
